9d301729193130d384d1f34fafda244f94ff1e23
Azizjon <azizjonnormatov437@gmail.com>
2023-09-04 08:13:46 +0500

    Create some function

--- src/main/java/com/example/validation/config/SecurityConfig.java
+++ src/main/java/com/example/validation/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
-
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
